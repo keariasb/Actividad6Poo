fcd957de7e367f2bde09f2ebe9d7389b8491272e
Kvnrs <keariasb@unal.edu.co>
2023-06-28 22:14:20 -0500

    commit inicial archivos

---
+++ Actividad6Kevin/src/Actividad6Kevin/main.java
@@ -0,0 +1,15 @@
+
+package Actividad6Kevin;
+
+/**
+ *
+ * @author Kevin
+ */
+public class main {
+    public static void main(String[] args) {
+        GUI interfaz = new GUI();
+        interfaz.setVisible(true);
+    }
+    
+    
+}
